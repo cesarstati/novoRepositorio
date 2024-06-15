914635b407c65fa044acbe719e2b80d7da1fcfbf
cesarstati <39418814+cesarstati@users.noreply.github.com>
2024-06-15 19:40:19 -0300

    Add files via upload
    Subindo arquivo

---
+++ 01_conversaoSegundos.py
@@ -0,0 +1,5 @@
+tempoMin=int(input('Digite o tempo em minutos: '))
+tempoConversao=tempoMin*60
+tempoSegundos=int(input('Digite o tempo em segundos: '))
+tempoTotalSegundos=tempoConversao+tempoSegundos
+print('O tempo',tempoMin,'minutos e',tempoSegundos,'segundos equivale a:',tempoTotalSegundos,'segundos')
